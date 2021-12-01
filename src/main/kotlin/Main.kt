e7d20b840a81a1220a2c9ce16a60e00cdae8aa9c
brisdalen <bwendelborgrisdalen@gmail.com>
2021-12-01 15:43:13 +0100

    Day 1 solution

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+import day1.day1
+import java.io.File
+
+fun main(args: Array<String>) {
+//    println(File("src/main/kotlin/day1/input.txt").readLines())
+
+    day1("src/main/kotlin/day1/input.txt")
+}
---
+++ src/main/kotlin/day1/day1.kt
@@ -0,0 +1,42 @@
+package day1
+
+import java.io.File
+import kotlin.streams.toList
+
+class day1(filepath: String) {
+
+    private val input = File(filepath).readLines()
+        .stream()
+        .mapToInt(String::toInt)
+        .toList()
+
+    init {
+        day1a()
+        day1b()
+    }
+
+    private fun day1a() {
+        var numIncreased = 0
+        for(i in 1 until input.size) {
+            if(input[i] > input[i-1]) {
+                numIncreased++
+            }
+        }
+        println(numIncreased)
+    }
+
+    private fun day1b() {
+        var sumsIncreased = 0
+        for(i in 0 until input.size - 3) {
+            // Compare N+1 to N
+            if(sum(i + 1, input) > sum(i, input)) {
+                sumsIncreased++
+            }
+        }
+        print(sumsIncreased)
+    }
+
+    private fun sum(i: Int, input: List<Int>) : Int {
+        return input[i] + input[i + 1] + input[i + 2]
+    }
+}
